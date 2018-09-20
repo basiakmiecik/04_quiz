f2644c651be160e8d9d9a9be4e1c73ff566d4b1d
Basia Kmiecik <basiakmiecik@gmail.com>
2018-09-20 15:55:45 +0200

    dodanie plikow z quizem

---
+++ src/test/ProgramUser.java
@@ -0,0 +1,12 @@
+package test;
+
+import pytania.Question;
+
+public class ProgramUser {
+    public static void main(String[] args) {
+
+        Question questions=new Question();
+        questions.question1();
+        questions.question2();
+        questions.question3();}
+}
